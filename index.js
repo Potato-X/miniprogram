210a2d4c20c53148b29e916b136e4c547ea4a89b
winner <921876203@qq.com>
2023-12-17 22:44:11 +0800

    add:添加node服务器

---
+++ index.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const logger = morgan("tiny");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(logger);
+
+// 首页
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// 更新计数
+app.post("/api/count", async (req, res) => {
+  const { action } = req.body;
+  if (action === "inc") {
+  } else if (action === "clear") {
+  }
+});
+
+// 获取计数
+app.get("/api/count", async (req, res) => {
+  res.send({
+    code: 0,
+    data: result,
+  });
+});
+
+// 小程序调用，获取微信 Open ID
+app.get("/api/wx_openid", async (req, res) => {
+  if (req.headers["x-wx-source"]) {
+    res.send(req.headers["x-wx-openid"]);
+  }
+});
+
+const port = process.env.PORT || 80;
+
+async function bootstrap() {
+  app.listen(port, () => {
+    console.log("启动成功", port);
+  });
+}
+
+bootstrap();
